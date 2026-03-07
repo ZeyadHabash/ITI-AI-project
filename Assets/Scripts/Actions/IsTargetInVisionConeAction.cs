ddc0985cf00eb5732b8b86d0e9e6935c1f1157c2
Adam Loay <adamlo2ay@gmail.com>
2026-03-07 22:09:07 +0200

    just kill me

--- Assets/Scripts/Actions/IsTargetInVisionConeAction.cs
+++ Assets/Scripts/Actions/IsTargetInVisionConeAction.cs
@@ -5,25 +5,28 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "IsTargetInVisionCone", story: "Is [Target] inside vision range [visionRange] and angle [visionAngle]", category: "Action/Conditional", id: "ff4448be221825d9d14cd70a36af94fa")]
-public partial class IsTargetInVisionConeAction : Action
+[NodeDescription(name: "IsTargetInVIsionCone", story: "Is [Target] in vision cone radius [radius] and angle [Angle] from [self]", category: "Action/Conditional", id: "b45d4f93eb28963a2fec0e64f03a4024")]
+public partial class IsTargetInVIsionConeAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Target;
-    [SerializeReference] public BlackboardVariable<float> VisionRange;
-    [SerializeReference] public BlackboardVariable<float> VisionAngle;
+    [SerializeReference] public BlackboardVariable<float> Radius;
+    [SerializeReference] public BlackboardVariable<float> Angle;
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
 
     protected override Status OnStart()
     {
-        return Status.Running;
-    }
+        if (Target.Value == null || Self.Value == null) return Status.Failure;
 
-    protected override Status OnUpdate()
-    {
+        Vector3 directionToTarget = Target.Value.transform.position - Self.Value.transform.position;
+
+
+        if (directionToTarget.magnitude > Radius.Value) return Status.Failure;
+
+
+        float angleToTarget = Vector3.Angle(Self.Value.transform.forward, directionToTarget);
+        if (angleToTarget > Angle.Value / 2f) return Status.Failure;
+
         return Status.Success;
-    }
-
-    protected override void OnEnd()
-    {
     }
 }
 
