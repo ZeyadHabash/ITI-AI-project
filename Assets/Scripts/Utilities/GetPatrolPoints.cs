13cb1a415a6f996486053f22f24d8bfce7f1ce8a
Osama <osamaaymanfayed@gmail.com>
2026-03-10 23:02:39 +0200

    Add GetPatrolPoints action & utility
    Introduce GetPatrolPointsAction and GetPatrolPoints utility to expose patrol waypoints to the behavior graph. Adds Assets/Scripts/Actions/GetPatrolPointsAction.cs and Assets/Scripts/Utilities/GetPatrolPoints.cs (with meta files), updates the DragonAI behavior asset to include the new action node, blackboard variables and ports, and updates the Osama_Testing scene to add a GetPatrolPoints component on the Points GameObject. The action finds the GameObject named "Points", reads its GetPatrolPoints component, and copies its Points list into a blackboard variable.

---
+++ Assets/Scripts/Utilities/GetPatrolPoints.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPatrolPoints : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> points;
+    public List<GameObject> Points => points;
+
+}
